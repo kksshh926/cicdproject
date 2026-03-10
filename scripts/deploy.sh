a58644d6292bc2f6ebe3fec991888bdb72cad9b4
kksshh926 <48307585+kksshh926@users.noreply.github.com>
2026-03-10 11:59:22 +0900

    cicd setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,26 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/cicdproject
+APP_NAME=cicdproject
+
 cd $REPOSITORY
 
-APP_NAME=cicdproject
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
-  echo "> 종료할것 없음."
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> 새 애플리케이션 배포"
+
+nohup java -jar $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
